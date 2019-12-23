b879aa2e6c87d8aa2814ed4f869511c76b47823e
Jacques Burns <jmathewburns@gmail.com>
2019-12-23 19:21:04 +0200

    Extract Command execution logic to separate file

--- index.js
+++ index.js
@@ -3,44 +3,17 @@
 const Discord = require('discord.js');
 
 const EnvironmentRepository = require('./src/context/EnvironmentRepository');
-const BotRequest = require('./src/commands/BotRequest');
-const CommandExecutionContext = require('./src/commands/CommandExecutionContext');
+const CommandExecutor = require('./src/commands/execution/CommandExecutor');
 
 const client = new Discord.Client();
-
-const COMMAND_PREFIX = process.env.COMMAND_PREFIX;
 
 client.on('ready', () => {
     console.log('Bot online.');
 });
 
 client.on('message', message => {
-    if (message.content.startsWith(COMMAND_PREFIX)) {
-        const environment = EnvironmentRepository.getEnvironment(message.channel.guild);
-
-        processCommand(environment, message);
-    }
+    const environment = EnvironmentRepository.getEnvironment(message.channel.guild);
+    CommandExecutor.execute(client, environment, message);
 });
-
-function processCommand(environment, message) {
-	if (!message.content.startsWith(environment.prefix) || message.author.bot) return;
-
-    const request = new BotRequest(COMMAND_PREFIX, message);
-
-    const command = environment.commands[request.commandName];
-
-    if (command) {
-        try {
-            command.action(new CommandExecutionContext(client, request, environment));
-        } catch (error) {
-            handleError(error, command.action, message.channel);
-        }
-    }
-}
-
-function handleError(e, backendMessage, channel) {
-    channel.send(`Error: ${e.message}`);
-    console.log(`Error (${e.message}): ${backendMessage}`);
-}
 
 client.login(process.env.BOT_TOKEN);
---
+++ src/commands/execution/CommandExecutor.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const BotRequest = require('./BotRequest');
+const CommandExecutionContext = require('./CommandExecutionContext');
+
+class CommandExecutor {
+    constructor() {
+        throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
+    }
+
+    static execute(client, environment, message) {
+        if (!message.content.startsWith(environment.prefix) || message.author.bot) return;
+
+        const request = new BotRequest(environment.prefix, message);
+        const command = environment.commands[request.commandName];
+
+        if (command) {
+            try {
+                command.action(new CommandExecutionContext(client, request, environment));
+            } catch (error) {
+                handleError(error, command.action, message.channel);
+            }
+        }
+    }
+}
+
+function handleError(e, backendMessage, channel) {
+    channel.send(`Error: ${e.message}`);
+    console.error(`Error (${e.message}): ${backendMessage}`);
+}
+
+module.exports = CommandExecutor;
