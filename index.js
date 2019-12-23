aa38c54820d473c7fa54f47b6d7cf107825d7a86
Jacques Burns <jmathewburns@gmail.com>
2019-12-23 16:55:55 +0200

    Cut support for reading from input files
    Until it can be tested to work properly, consistently

--- index.js
+++ index.js
@@ -48,18 +48,12 @@ function initialiseBaseCommands() {
             }
             request.message.channel.send(response);
         }, 'Prints a list of the currently registered (base and custom) commands along with a description, if any'),
-        'registerfile': new Command('registerfile', function(_client, request, environment) {
-            registerFunctionFromFile(request.message.channel, request.terms, environment);
-        }, `
-        Registers the given file as a command. Parameter 1 is the desired command name, parameter 2 is a URL to the file. File extension does not matter.
-        Example: \`!registerfile example https://example.org/path/to/json/function.js\`
-        `),
-        'registerinline': new Command('registerinline', function(_client, request, environment) {
+        'register': new Command('register', function(_client, request, environment) {
             registerFunctionFromInlineSource(request.message.channel, request.arguments, request.terms, environment);
         }, `
         Registers the given JavaScript function source code as a command. Parameter 1 is the desired command name, the rest of the message should be 
         the function source, not surrounded in quotes.
-        Example: \`!registerinline example function(client, request, environment) { request.message.channel.send("Example"); }\`
+        Example: \`!register example function(client, request, environment) { request.message.channel.send("Example"); }\`
         `),
     };
 }
@@ -73,39 +67,6 @@ function registerFunctionFromInlineSource(channel, messageContent, terms, enviro
     } catch (error) {
         handleError(error, rawFunction, channel);
     }
-}
-
-function registerFunctionFromFile(channel, terms, environment) {
-    const command = terms[1];
-    const url = terms[2];
-    let fileContent;
-    try {
-        fileContent = downloadFile(url);
-        registerFunction(fileContent, command, environment);
-        channel.send('Added command: ' + COMMAND_PREFIX + command);
-    } catch (error) {
-        if (!fileContent) {
-            fileContent = url;
-        }
-        handleError(error, fileContent, channel);
-    }
-}
-
-function downloadFile(url) {
-    let content;
-    let error;
-
-    Axios.get(url).then(response => {
-        content = response.data;
-    }).catch(e => {
-        error = e;
-    });
-
-    if (error) {
-        throw error;
-    }
-
-    return content;
 }
 
 function registerFunction(functionSource, functionName, environment) {
